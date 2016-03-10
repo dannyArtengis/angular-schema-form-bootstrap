10bf8eacda60dbf5d4ebc765574848db6721a90d
Daniel Schild <daniel.schild@artengis.com>
2016-03-10 14:30:05 +0100

    - added md tab

--- bootstrap-decorator.js
+++ bootstrap-decorator.js
@@ -3,7 +3,7 @@ $templateCache.put("decorators/bootstrap/actions.html","<div class=\"btn-group s
 $templateCache.put("decorators/bootstrap/array.html","<div class=\"schema-form-array {{form.htmlClass}}\" sf-field-model=\"sf-new-array\" sf-new-array=\"\"><label class=\"control-label\" ng-show=\"showTitle()\">{{ form.title }}</label><ol class=\"list-group\" sf-field-model=\"\" ui-sortable=\"form.sortOptions\"><li class=\"list-group-item {{form.fieldHtmlClass}}\" schema-form-array-items=\"\" sf-field-model=\"ng-repeat\" ng-repeat=\"item in $$value$$ track by $index\"><button ng-hide=\"form.readonly || form.remove === null\" ng-click=\"deleteFromArray($index)\" ng-disabled=\"form.schema.minItems >= modelArray.length\" style=\"position: relative; z-index: 20;\" type=\"button\" class=\"close pull-right\"><span aria-hidden=\"true\">&times;</span><span class=\"sr-only\">Close</span></button></li></ol><div class=\"clearfix\" style=\"padding: 15px;\" ng-model=\"modelArray\" schema-validate=\"form\"><div class=\"help-block\" ng-show=\"(hasError() && errorMessage(schemaError())) || form.description\" ng-bind-html=\"(hasError() && errorMessage(schemaError())) || form.description\"></div><button ng-hide=\"form.readonly || form.add === null\" ng-click=\"appendToArray()\" ng-disabled=\"form.schema.maxItems <= modelArray.length\" type=\"button\" class=\"btn {{ form.style.add || \'btn-default\' }} pull-right\"><i class=\"glyphicon glyphicon-plus\"></i> {{ form.add || \'Add\'}}</button></div></div>");
 $templateCache.put("decorators/bootstrap/checkbox.html","<div class=\"checkbox schema-form-checkbox {{::form.htmlClass}}\" ng-class=\"{\'has-error\': hasError(), \'has-success\': hasSuccess()}\" sf-messages=\"\"><md-checkbox sf-field-model=\"\" sf-changed=\"form\" ng-disabled=\"form.readonly\" schema-validate=\"form\" class=\"{{::form.fieldHtmlClass}}\" name=\"{{::form.key|sfCamelKey}}\" aria-label=\"{{::form.title}}\"><span>{{::form.title}}</span></md-checkbox></div>");
 $templateCache.put("decorators/bootstrap/checkboxes.html","<div sf-field-model=\"sf-new-array\" sf-new-array=\"\" class=\"form-group schema-form-checkboxes {{form.htmlClass}}\" ng-class=\"{\'has-error\': form.disableErrorState !== true && hasError(), \'has-success\': form.disableSuccessState !== true && hasSuccess()}\"><label class=\"control-label {{form.labelHtmlClass}}\" sf-field-model=\"\" schema-validate=\"form\" ng-show=\"showTitle()\">{{form.title}}</label><div class=\"checkbox\" ng-repeat=\"val in titleMapValues track by $index\"><label><input type=\"checkbox\" ng-disabled=\"form.readonly\" sf-changed=\"form\" class=\"{{form.fieldHtmlClass}}\" ng-model=\"titleMapValues[$index]\" name=\"{{form.key.slice(-1)[0]}}\"> <span ng-bind-html=\"form.titleMap[$index].name\"></span></label></div><div class=\"help-block\" sf-message=\"form.description\"></div></div>");
-$templateCache.put("decorators/bootstrap/default.html","<div class=\"form-group schema-form-{{form.type}} {{form.htmlClass}}\" ng-class=\"{\'has-error\': form.disableErrorState !== true && hasError(), \'has-success\': form.disableSuccessState !== true && hasSuccess(), \'has-feedback\': form.feedback !== false }\"><label class=\"control-label {{form.labelHtmlClass}}\" ng-class=\"{\'sr-only\': !showTitle()}\" for=\"{{form.key.slice(-1)[0]}}\">{{form.title}}</label> <input ng-if=\"!form.fieldAddonLeft && !form.fieldAddonRight\" ng-show=\"form.key\" type=\"{{form.type}}\" step=\"any\" sf-changed=\"form\" placeholder=\"{{form.placeholder}}\" class=\"form-control {{form.fieldHtmlClass}}\" id=\"{{form.key.slice(-1)[0]}}\" sf-field-model=\"\" ng-disabled=\"form.readonly\" schema-validate=\"form\" name=\"{{form.key.slice(-1)[0]}}\" aria-describedby=\"{{form.key.slice(-1)[0] + \'Status\'}}\"><div ng-if=\"form.fieldAddonLeft || form.fieldAddonRight\" ng-class=\"{\'input-group\': (form.fieldAddonLeft || form.fieldAddonRight)}\"><span ng-if=\"form.fieldAddonLeft\" class=\"input-group-addon\" ng-bind-html=\"form.fieldAddonLeft\"></span> <input ng-show=\"form.key\" type=\"{{form.type}}\" step=\"any\" sf-changed=\"form\" placeholder=\"{{form.placeholder}}\" class=\"form-control {{form.fieldHtmlClass}}\" id=\"{{form.key.slice(-1)[0]}}\" sf-field-model=\"\" ng-disabled=\"form.readonly\" schema-validate=\"form\" name=\"{{form.key.slice(-1)[0]}}\" aria-describedby=\"{{form.key.slice(-1)[0] + \'Status\'}}\"> <span ng-if=\"form.fieldAddonRight\" class=\"input-group-addon\" ng-bind-html=\"form.fieldAddonRight\"></span></div><span ng-if=\"form.feedback !== false\" class=\"form-control-feedback\" ng-class=\"evalInScope(form.feedback) || {\'glyphicon\': true, \'glyphicon-ok\': form.disableSuccessState !== true && hasSuccess(), \'glyphicon-remove\': form.disableErrorState !== true && hasError() }\" aria-hidden=\"true\"></span> <span ng-if=\"hasError() || hasSuccess()\" id=\"{{form.key.slice(-1)[0] + \'Status\'}}\" class=\"sr-only\">{{ hasSuccess() ? \'(success)\' : \'(error)\' }}</span><div class=\"help-block\" sf-message=\"form.description\"></div></div>");
+$templateCache.put("decorators/bootstrap/default.html","<md-input-container class=\"schema-form-{{::form.type}} {{::form.htmlClass}}\" ng-class=\"{\'md-input-has-value\': model[\'{{form.key.join(\'\\\'][\\\'\')}}\'], \'has-error\': hasError(), \'has-success\': hasSuccess(), \'has-feedback\': form.feedback !== false}\" sf-messages=\"\" sf-layout=\"\"><label ng-show=\"showTitle()\" for=\"{{::form.key|sfCamelKey}}\">{{::form.title}}</label> <input sf-field-model=\"\" ng-show=\"::form.key\" type=\"{{::form.type}}\" step=\"any\" sf-changed=\"form\" placeholder=\"{{form.placeholder}}\" id=\"{{::form.key|sfCamelKey}}\" ng-class=\"::form.fieldHtmlClass\" sf-type-parser=\"form.schema\" ng-disabled=\"::form.readonly\" schema-validate=\"form\" name=\"{{::form.key|sfCamelKey}}\" aria-describedby=\"{{::form.key|sfCamelKey}}Status\"></md-input-container>");
 $templateCache.put("decorators/bootstrap/fieldset.html","<fieldset ng-disabled=\"form.readonly\" class=\"schema-form-fieldset {{form.htmlClass}}\"><legend ng-class=\"{\'sr-only\': !showTitle() }\">{{ form.title }}</legend><div class=\"help-block\" ng-show=\"form.description\" ng-bind-html=\"form.description\"></div></fieldset>");
 $templateCache.put("decorators/bootstrap/help.html","<div class=\"helpvalue schema-form-helpvalue {{form.htmlClass}}\" ng-bind-html=\"form.helpvalue\"></div>");
 $templateCache.put("decorators/bootstrap/radio-buttons.html","<div class=\"form-group schema-form-radiobuttons {{form.htmlClass}}\" ng-class=\"{\'has-error\': form.disableErrorState !== true && hasError(), \'has-success\': form.disableSuccessState !== true && hasSuccess()}\"><div><label class=\"control-label {{form.labelHtmlClass}}\" ng-show=\"showTitle()\">{{form.title}}</label></div><div class=\"btn-group\"><label sf-field-model=\"replaceAll\" class=\"btn {{ (item.value === $$value$$) ? form.style.selected || \'btn-default\' : form.style.unselected || \'btn-default\'; }}\" ng-class=\"{ active: item.value === $$value$$ }\" ng-repeat=\"item in form.titleMap\"><input type=\"radio\" class=\"{{form.fieldHtmlClass}}\" sf-changed=\"form\" style=\"display: none;\" ng-disabled=\"form.readonly\" sf-field-model=\"\" schema-validate=\"form\" ng-value=\"item.value\" name=\"{{form.key.join(\'.\')}}\"> <span ng-bind-html=\"item.name\"></span></label></div><div class=\"help-block\" sf-message=\"form.description\"></div></div>");
@@ -29,19 +29,24 @@ $templateCache.put("decorators/bootstrap/textarea.html","<div class=\"form-group
       var sfField = sfBuilderProvider.builders.sfField;
       var condition = sfBuilderProvider.builders.condition;
       var array = sfBuilderProvider.builders.array;
-
       var defaults = [sfField, ngModel, ngModelOptions, condition];
 
+      // MD
       var mdTabs = mdTabsBuilder;
+      var sfLayout           = sfLayout;
+      var sfMessagesNode     = sfMessagesNodeHandler();
+      var sfMessages         = sfMessagesBuilder;
+      var core = [ sfField, ngModel, ngModelOptions, condition, sfLayout ];
+      var mdDefaults = core.concat(sfMessages);
 
       decoratorsProvider.defineDecorator('bootstrapDecorator', {
         actions: {template: base + 'actions.html', builder: defaults},
         array: {template: base + 'array.html', builder: [sfField, ngModelOptions, ngModel, array, condition]},
         button: {template: base + 'submit.html', builder: defaults},
-        checkbox: {template: base + 'checkbox.html', builder: defaults},
+        checkbox: {template: base + 'checkbox.html', builder: defaults}, // MD
         checkboxes: {template: base + 'checkboxes.html', builder: [sfField, ngModelOptions, ngModel, array, condition]},
         conditional: {template: base + 'section.html', builder: [sfField, simpleTransclusion, condition]},
-        'default': {template: base + 'default.html', builder: defaults},
+        'default': {template: base + 'default.html', builder: mdDefaults},  // MD
         fieldset: {template: base + 'fieldset.html', builder: [sfField, simpleTransclusion, condition]},
         help: {template: base + 'help.html', builder: defaults},
         number: {template: base + 'default.html', builder: defaults},
@@ -53,7 +58,7 @@ $templateCache.put("decorators/bootstrap/textarea.html","<div class=\"form-group
         select: {template: base + 'select.html', builder: defaults},
         submit: {template: base + 'submit.html', builder: defaults},
         tabarray: {template: base + 'tabarray.html', builder: [sfField, ngModelOptions, ngModel, array, condition]},
-        tabs: {template: base + 'tabs.html', builder: [sfField, mdTabs, condition]},
+        tabs: {template: base + 'tabs.html', builder: [sfField, mdTabs, condition]}, // MD
         textarea: {template: base + 'textarea.html', builder: defaults}
       }, []);
 
@@ -70,6 +75,28 @@ $templateCache.put("decorators/bootstrap/textarea.html","<div class=\"form-group
             mdTab.appendChild(mdTabBody);
             mdTabsFrag.appendChild(mdTab);
           });
+        }
+      };
+      function sfLayout(args) {
+        var layoutDiv = args.fieldFrag.querySelector('[sf-layout]');
+
+        if (args.form.grid) {
+          Object.getOwnPropertyNames(args.form.grid).forEach(function(property, idx, array) {
+            layoutDiv.setAttribute(property, args.form.grid[property]);
+          });
+        };
+      };
+      function sfMessagesNodeHandler() {
+        var html = '<div ng-if="ngModel.$invalid" ng-messages="ngModel.$error"><div sf-message ng-message></div></div>';
+        var div = document.createElement('div');
+        div.innerHTML = html;
+        return div.firstChild;
+      };
+      function sfMessagesBuilder(args) {
+        var messagesDiv = args.fieldFrag.querySelector('[sf-messages]');
+        if (messagesDiv && sfMessagesNode) {
+          var child = sfMessagesNode.cloneNode();
+          messagesDiv.appendChild(child);
         }
       };
     }
--- src/bootstrap-decorator.js
+++ src/bootstrap-decorator.js
@@ -12,19 +12,24 @@
       var sfField = sfBuilderProvider.builders.sfField;
       var condition = sfBuilderProvider.builders.condition;
       var array = sfBuilderProvider.builders.array;
-
       var defaults = [sfField, ngModel, ngModelOptions, condition];
 
+      // MD
       var mdTabs = mdTabsBuilder;
+      var sfLayout           = sfLayout;
+      var sfMessagesNode     = sfMessagesNodeHandler();
+      var sfMessages         = sfMessagesBuilder;
+      var core = [ sfField, ngModel, ngModelOptions, condition, sfLayout ];
+      var mdDefaults = core.concat(sfMessages);
 
       decoratorsProvider.defineDecorator('bootstrapDecorator', {
         actions: {template: base + 'actions.html', builder: defaults},
         array: {template: base + 'array.html', builder: [sfField, ngModelOptions, ngModel, array, condition]},
         button: {template: base + 'submit.html', builder: defaults},
-        checkbox: {template: base + 'checkbox.html', builder: defaults},
+        checkbox: {template: base + 'checkbox.html', builder: defaults}, // MD
         checkboxes: {template: base + 'checkboxes.html', builder: [sfField, ngModelOptions, ngModel, array, condition]},
         conditional: {template: base + 'section.html', builder: [sfField, simpleTransclusion, condition]},
-        'default': {template: base + 'default.html', builder: defaults},
+        'default': {template: base + 'default.html', builder: mdDefaults},  // MD
         fieldset: {template: base + 'fieldset.html', builder: [sfField, simpleTransclusion, condition]},
         help: {template: base + 'help.html', builder: defaults},
         number: {template: base + 'default.html', builder: defaults},
@@ -36,7 +41,7 @@
         select: {template: base + 'select.html', builder: defaults},
         submit: {template: base + 'submit.html', builder: defaults},
         tabarray: {template: base + 'tabarray.html', builder: [sfField, ngModelOptions, ngModel, array, condition]},
-        tabs: {template: base + 'tabs.html', builder: [sfField, mdTabs, condition]},
+        tabs: {template: base + 'tabs.html', builder: [sfField, mdTabs, condition]}, // MD
         textarea: {template: base + 'textarea.html', builder: defaults}
       }, []);
 
@@ -53,6 +58,28 @@
             mdTab.appendChild(mdTabBody);
             mdTabsFrag.appendChild(mdTab);
           });
+        }
+      };
+      function sfLayout(args) {
+        var layoutDiv = args.fieldFrag.querySelector('[sf-layout]');
+
+        if (args.form.grid) {
+          Object.getOwnPropertyNames(args.form.grid).forEach(function(property, idx, array) {
+            layoutDiv.setAttribute(property, args.form.grid[property]);
+          });
+        };
+      };
+      function sfMessagesNodeHandler() {
+        var html = '<div ng-if="ngModel.$invalid" ng-messages="ngModel.$error"><div sf-message ng-message></div></div>';
+        var div = document.createElement('div');
+        div.innerHTML = html;
+        return div.firstChild;
+      };
+      function sfMessagesBuilder(args) {
+        var messagesDiv = args.fieldFrag.querySelector('[sf-messages]');
+        if (messagesDiv && sfMessagesNode) {
+          var child = sfMessagesNode.cloneNode();
+          messagesDiv.appendChild(child);
         }
       };
     }
